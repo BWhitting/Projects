11e081c00097e29d1046806f50ccf15085a393ba
BWhitting <whitting_briana@students.columbusstate.edu>
2025-01-18 20:42:40 -0500

    Create First.java

---
+++ First.java
@@ -0,0 +1,9 @@
+// Author: Briana P Whitting
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Job Title: Computer Sytems Analyst");
+        System.out.println("I am passionate about solving complex problems and creating efficient solutions.");
+        System.out.println("Median income: $110,000");
+    }
+}
